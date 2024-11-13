dcc5d0bafafd7381df96f87cc8e091ed1e52f808
fernandobouchet <fernandobouchet@gmail.com>
2024-11-12 22:48:25 -0300

    feat: update 'auth' file configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,32 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import { prisma } from "@/prisma/prisma";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token.email && typeof token.id === "string") {
+        session.user.id = token.id;
+        session.user.email = token.email;
+      }
+      return session;
+    },
+  },
+  secret: process.env.AUTH_SECRET,
 });
