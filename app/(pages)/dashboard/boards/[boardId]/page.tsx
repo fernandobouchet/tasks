76d955d63d8c7b8217212d7d8357eb4aa3cdc02b
fernandobouchet <fernandobouchet@gmail.com>
2024-11-24 21:08:09 -0300

    feat: throw a 'not found' page when dinamic route doesnt exists

--- app/(pages)/dashboard/boards/[boardId]/page.tsx
+++ app/(pages)/dashboard/boards/[boardId]/page.tsx
@@ -2,17 +2,21 @@ import { TaskFormDialog } from "@/components/task/form/taskFormDialog";
 import { columns } from "@/components/task/table/columns";
 import { DataTable } from "@/components/task/table/dataTable";
 import { getAllTaskFromBoardId } from "@/lib/actions/task/getAllTaskFromBoardId";
+import { notFound } from "next/navigation";
 
 interface Props {
   params: {
     boardId: string;
-    shortName: string;
   };
 }
 
 export default async function Board({ params }: Props) {
   const boardId = params.boardId;
   const tasks = await getAllTaskFromBoardId(boardId);
+
+  if (!tasks) {
+    return notFound();
+  }
 
   return (
     <div>
--- lib/actions/task/getAllTaskFromBoardId.ts
+++ lib/actions/task/getAllTaskFromBoardId.ts
@@ -1,12 +1,24 @@
 import { prisma } from "@/prisma/prisma";
 
 const getAllTaskFromBoardId = async (boardId: string) => {
-  const data = await prisma.task.findMany({
-    where: {
-      boardId,
-    },
-  });
-  return data;
+  try {
+    const board = await prisma.board.findUnique({
+      where: {
+        boardId: boardId,
+      },
+    });
+
+    if (!board) return null;
+
+    const data = await prisma.task.findMany({
+      where: {
+        boardId,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { getAllTaskFromBoardId };
